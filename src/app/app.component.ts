74f832f1533d34085fe2f2cb2066e4cc77e31bce
gayetalib <gayetalib90@gmail.com>
2025-04-09 11:29:46 +0000

    add cmd and routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cash_management_front';
+  age: number = 30;
+  counter: number = 0;
+  value: string = '';
+  display: boolean = false;
+  onEdit(value: string) {
+    this.value = value == '' ? '' : 'Hello ' + value + '!';
+  }
+
+  increment() {
+    this.counter++;
+  }
+
+  decrement() {
+    this.counter--;
+  }
+
+  reset() {
+    this.counter = 0;
+  }
+
 }
